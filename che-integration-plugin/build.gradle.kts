9f6b8eee980185e9920e68fbd9c3bd1d39bfb240
Artem Zatsarynnyi <azatsary@redhat.com>
2025-11-07 15:30:12 +0100

    Stop DevWorkspace by the user inactivity timeout (#57)
    * Stop DevWorkspace by the user inactivity timeout
    
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>
    
    * Stop DevWorkspace by the user inactivity timeout
    
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>
    
    * Update .gitignore
    
    * Update the README with steps for development
    
    Added development instructions for testing changes
    
    * Add a note to the Dockerfile
    
    ---------
    
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

---
+++ che-integration-plugin/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.10.1"
+}
+
+group = "io.github.che.integration"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+// Configure IntelliJ Platform Gradle Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    intellijPlatform {
+        create("IC", "2025.1.4.1")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+        // Add necessary plugin dependencies for compilation here, example:
+        // bundledPlugin("com.intellij.java")
+    }
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "251"
+        }
+
+        changeNotes = """
+            Initial version
+        """.trimIndent()
+    }
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
---
+++ che-integration-plugin/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "che-integration-plugin"
